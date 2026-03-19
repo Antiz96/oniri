40849df45fc2f53b3815c79c1302ee204358143b
Robin Candau <robincandau@protonmail.com>
2026-03-19 10:45:23 +0100

    lint

--- src/help.rs
+++ src/help.rs
@@ -13,8 +13,12 @@ pub fn show_help() -> bool {
         println!();
         println!("OPTIONS:");
         println!("  -F, --first-only        Only act on the first window");
-        println!("  -H, --height-tolerance  Set the height size tolerance (in pixels) when comparing the window size to the output size to determine if the window is maximized or not");
-        println!("  -W, --width-tolerance   Set the width size tolerance (in pixels) when comparing the window size to the output size to determine if the window is maximized or not");
+        println!(
+            "  -H, --height-tolerance  Set the height size tolerance (in pixels) when comparing the window size to the output size to determine if the window is maximized or not"
+        );
+        println!(
+            "  -W, --width-tolerance   Set the width size tolerance (in pixels) when comparing the window size to the output size to determine if the window is maximized or not"
+        );
         println!("  -h, --help              Display this help message");
         println!("  -V, --version           Display version information");
         println!();
