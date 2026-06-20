31d32e4ffef032b22431c0fce165b30bbd7d9ac5
Robin Candau <robincandau@protonmail.com>
2026-06-21 00:55:19 +0200

    chore: Add test for the help argument (#71)

---
+++ tests/help.rs
@@ -0,0 +1,10 @@
+//! Test the --help argument
+
+use assert_cmd::Command;
+
+#[test]
+fn help_arg() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+
+    cmd.arg("--help").assert().success();
+}
