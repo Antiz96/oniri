7f3bba292b50fa5d89ee92baf96a648db7143d9c
Robin Candau <robincandau@protonmail.com>
2026-07-12 19:08:45 +0200

    chore: Drop useless line breaks (#88)

--- src/socket_connections.rs
+++ src/socket_connections.rs
@@ -7,20 +7,20 @@ use std::process;
 pub fn init_socket_connections() -> anyhow::Result<(Socket, Socket)> {
     // Connect to niri IPC socket
     let mut event_socket = Socket::connect().unwrap_or_else(|error| {
-        error!("Failed to connect to niri IPC socket:\n{error}");
+        error!("Failed to connect to niri IPC socket: {error}");
         process::exit(2);
     });
 
     // Start the event stream
     let reply = event_socket.send(Request::EventStream)?;
     if !matches!(reply, Ok(Response::Handled)) {
-        error!("Failed to start event stream:\n{:?}", reply);
+        error!("Failed to start event stream: {:?}", reply);
         return Err(anyhow::anyhow!(""));
     }
 
     // Create a separate socket connection to send actions
     let action_socket = Socket::connect().unwrap_or_else(|error| {
-        error!("Failed to connect to niri IPC socket:\n{error}");
+        error!("Failed to connect to niri IPC socket: {error}");
         process::exit(2);
     });
 
